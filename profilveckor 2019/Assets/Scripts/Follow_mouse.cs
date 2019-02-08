c8fd7dbd8ba01c67268dc4c20546b467449b378c
Presidentmoose2 <33448125+Presidentmoose2@users.noreply.github.com>
2019-02-08 11:29:36 +0100

    Update Follow_mouse.cs

--- profilveckor 2019/Assets/Scripts/Follow_mouse.cs
+++ profilveckor 2019/Assets/Scripts/Follow_mouse.cs
@@ -5,24 +5,24 @@ public class Follow_mouse : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
-        follow_Mouse();
+        Follow_Mouse();
 
 
 
     }
 
     // Update is called once per frame
-    void follow_Mouse()
+    void Follow_Mouse()
     {
         bool v = gameObject.tag == "Player";
 
         if (v)
         {
             Vector2 mousePosition = Input.mousePosition;
-            Vector2 characterScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+            Vector2 characterScreenPos = Camera.main.WorldToScreenPoint(transform.GetChild(0).position);
             Vector2 direction = mousePosition - characterScreenPos;
-            
-            transform.right = direction;
+
+            transform.GetChild(0).localRotation = Quaternion.Euler(transform.GetChild(0).localRotation.eulerAngles.x, -Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, transform.GetChild(0).localRotation.eulerAngles.z);
 
         }
              
